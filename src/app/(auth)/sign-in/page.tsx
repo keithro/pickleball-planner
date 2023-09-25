6642c245faeac3468e0d12856ba592c721e19a90
keithro <rodriguez.keith@gmail.com>
2023-09-24 21:17:01 -0500

    Saving before trying shadcn

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -6,7 +6,7 @@ const Page = () => {
   return (
     <div className="absolute inset-0">
       <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20">
-        <Link href="/">
+        <Link href="/" className="self-start -mt-20 flex">
           <ChevronLeft className="" />
           Home
         </Link>
--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,8 +1,42 @@
+"use client";
+
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+import { Loader2 } from "lucide-react";
+import { Icons } from "./Icons";
+
 const UserAuthForm = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
+
+    try {
+      await signIn("google");
+    } catch (error) {
+      // TODO: toast notification
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <>
-      <button type="button">Log In</button>
-    </>
+    <div className="flex justify-center">
+      <button
+        type="button"
+        className="w-full flex"
+        onClick={loginWithGoogle}
+        disabled={isLoading}
+      >
+        {isLoading ? (
+          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+        ) : (
+          <Icons.google className="mr-2 h-4 w-4" />
+        )}
+        Log In
+      </button>
+    </div>
   );
 };
 
